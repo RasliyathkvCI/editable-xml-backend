30b755a5113e896445967dadcacd4d56fc66065c
RasliyathkvCI <rasliyath.kollavalappil@citrusinformatics.in>
2025-05-19 16:15:58 +0530

    fix : server code

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 // Middlewares
 app.use(cors());
 app.use(express.json());
-
+app.set('trust proxy', true);  
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
